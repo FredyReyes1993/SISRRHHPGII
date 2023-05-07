71dd302e428cd7c7ffe904627d91289b9a77f07d
FredyReyes1993 <fredyreyes40273848@gmail.com>
2023-05-07 00:29:54 -0600

    primera version

---
+++ reportesClientes/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+
+# Create your tests here.
+        #print(clientes.query) # imprime la consulta SQL generada por Django
+        #print(clientes)
+        #print(fecha_inicio)
+        #print(fecha_fin)
